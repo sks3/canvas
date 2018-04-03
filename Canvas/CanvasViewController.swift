ada049001b502d2cf0cb8d66802da80361a7f929
sks3 <saketsingh2018@gmail.com>
2018-04-02 19:59:03 -0500

    Pan faces to canvas

--- Canvas/CanvasViewController.swift
+++ Canvas/CanvasViewController.swift
@@ -13,13 +13,46 @@ class CanvasViewController: UIViewController {
   @IBOutlet var trayView: UIView!
   
   var trayOriginalCenter: CGPoint!
+  var trayDownOffset: CGFloat!
+  var trayUp: CGPoint!
+  var trayDown: CGPoint!
+  var newlyCreatedFace: UIImageView!
+  var newlyCreatedFaceOriginalCenter: CGPoint!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    trayDownOffset = 240
+    trayUp = trayView.center
+    trayDown = CGPoint(x: trayView.center.x, y: trayView.center.y + trayDownOffset)
+    
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  
+  @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+    let translation = sender.translation(in: view)
+    
+    if sender.state == .began {
+      var imageView = sender.view as! UIImageView
+      newlyCreatedFace = UIImageView(image: imageView.image)
+      view.addSubview(newlyCreatedFace)
+      newlyCreatedFace.center = imageView.center
+      newlyCreatedFace.center.y += trayView.frame.origin.y
+      newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+    }
+    else if sender.state == .changed {
+      newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+    }
+    
+  }
+  
   
   
   @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
     let translation = sender.translation(in: view)
     var velocity = sender.velocity(in: view)
-    
-    
     
     if sender.state == .began {
       trayOriginalCenter = trayView.center
@@ -28,17 +61,24 @@ class CanvasViewController: UIViewController {
       trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
     }
     else if sender.state == .ended {
+      if velocity.y > 0 {
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: [], animations: { () -> Void in
+          self.trayView.center = self.trayDown
+        }, completion: nil)
+      }
+      else {
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: [], animations: { () -> Void in
+          self.trayView.center = self.trayUp
+        }, completion: nil)
+      }
+      
       trayOriginalCenter = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
     }
   }
   
   
   
-  override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
